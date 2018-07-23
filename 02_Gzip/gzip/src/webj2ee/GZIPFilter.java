aabd2f3555faf67696c0664f32843fb90ea84b85
webj2ee <1696627335@qq.com>
2018-07-23 22:00:21 +0800

    精简代码

--- 02_Gzip/gzip/src/webj2ee/GZIPFilter.java
+++ 02_Gzip/gzip/src/webj2ee/GZIPFilter.java
@@ -15,24 +15,110 @@ package webj2ee;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Enumeration;
 import java.util.zip.GZIPOutputStream;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebFilter(filterName = "GZIPFilter", urlPatterns = "*.jsp")
-public class GZIPFilter extends GenericFilter {
+public class GZIPFilter implements Filter {
+	
+	
+	
+	/** The filter configuration provided by the container */
+	private FilterConfig filterConfig;
+
+	/** {@inheritDoc} */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		doFilterInternal((HttpServletRequest) request, (HttpServletResponse) response, chain);
+	}
+	
+	/**
+	 * Returns the <code>FilterConfig</code> for this filter's web.xml registration.
+	 * 
+	 * @return the <code>FilterConfig</code> for this filter's web.xml registration
+	 */
+	public FilterConfig getFilterConfig() {
+		return filterConfig;
+	}
+
+	/**
+	 * Sets the <code>FilterConfig</code> for this filter's web.xml registration.
+	 * 
+	 * @param val the <code>FilterConfig</code> for this filter's web.xml
+	 *            registration. This is required by WebLogic 6.1's filter
+	 *            implementation.
+	 */
+	public void setFilterConfig(FilterConfig val) {
+		init(val);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void init(FilterConfig val) {
+		filterConfig = val;
+		printBanner(val.getServletContext());
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		filterConfig = null;
+	}
+
+	/**
+	 * Utility method to examine the given request, find the given header and see if
+	 * the header contains the provided value
+	 * 
+	 * @param request the request to examine
+	 * @param header  the specific header to check the value on
+	 * @param value   the value to look for in the header
+	 * @return boolean true if the header contains the given value
+	 */
+	protected boolean headerContains(HttpServletRequest request, String header, String value) {
+		Enumeration accepted = request.getHeaders(header);
+		while (accepted.hasMoreElements()) {
+			String headerValue = (String) accepted.nextElement();
+			if (headerValue.indexOf(value) != -1) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	
+	
 	/** The header for accepted encodings */
 	public static final String ACCEPT_ENCODING = "Accept-Encoding";
 
 	/** The header for content encodings */
 	public static final String CONTENT_ENCODING = "Content-Encoding";
 
+	
+	
+	/**
+	 * Performs the actual filtering actions
+	 *
+	 * @param request  the request to work with
+	 * @param response the response to work with
+	 * @param chain    the filter chain to work with
+	 * @exception IOException      if there is a problem performing IO
+	 * @exception ServletException general servlet exceptions
+	 */
 	/**
 	 * Filters the response by wrapping the output stream, by checking to see if the
 	 * browser will accept gzip encodings, and if they can, temporarily storing all
@@ -83,7 +169,12 @@ public class GZIPFilter extends GenericFilter {
 		}
 	}
 
-	/** {@inheritDoc} */
+	/**
+	 * Print a configuration banner including your CVS Version, as well as any
+	 * configuration information you have, to the given context's log
+	 * 
+	 * @param context ServletContext object to use for logging
+	 */
 	public void printBanner(ServletContext context) {
 		context.log("$Id: GZIPFilter.java,v 1.15 2005/03/12 01:52:29 mike Exp $");
 		context.log("\tGZIPFilter.Enabled: " + isEnabled());
